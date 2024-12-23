178e5b3267febe041347b2572876847ebba6834f
Hayden <hchalin98@gmail.com>
2024-12-23 16:09:44 -0500

    Initial Commit

---
+++ HelloTriangle/HelloTriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTriangleApp.swift
+//  HelloTriangle
+//
+//  Created by Hayden Chalin on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
